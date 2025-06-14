310b33c198e3783a51ba0cc833938fdb05e13b5f
Adarsh Mohan P <adarshmohanelayad@gmail.com>
2025-06-14 02:18:48 -0400

    updated

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,7 +2,11 @@ const btnSearch = document.getElementById("btnSearch");
 const btnClear = document.getElementById("btnClear");
 
 function searchDestination(){
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
 
+    
 }
 function clearDestination(){
 
