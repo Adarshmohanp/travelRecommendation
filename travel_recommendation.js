cdceac7790daf20a50ea3ff0225f0a2c13f7992e
Adarsh Mohan P <adarshmohanelayad@gmail.com>
2025-06-14 02:05:41 -0400

    updated

---
+++ travel_recommendation.js
@@ -0,0 +1,13 @@
+const btnSearch = document.getElementById("btnSearch");
+const btnClear = document.getElementById("btnClear");
+
+function searchDestination(){
+
+}
+function clearDestination(){
+
+}
+btnSearch.addEventListener('click',searchDestination);
+btnSearch.addEventListener('click',clearDestination);
+
+
